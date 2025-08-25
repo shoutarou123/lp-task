5cbd5c1b84b479991d64fe0bf229661c26d657ed
shotaro <850123.a@gmail.com>
2025-08-25 23:36:08 +0900

    fv-pc-complete

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>taiziii </title>
+  <link rel="stylesheet" href="./css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Protest+Strike&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+  <section class="fv">
+    <div class="bg_img">
+      <div class="logo_content_flex">
+        <img class="logo_img" src="./img/pc/グループ 18 1.png" />
+        <img class="button_img" src="./img/pc/Group 5.png" />
+      </div>
+
+      <div class="sub_title_content">
+        <p class="sub_title_text">現場に眠るノウハウをDXで活かしたい経営層・部門責任者・人事担当の方へ</p>
+      </div>
+      <div class="catchphrase_img_content">
+        <img class="catchphrase_img" src="./img/pc/キャッチコピー@2x 2.png" />
+      </div>
+      <div class="intai_text_content">
+        <p class="intai_text">引退・異動で失われる前に、知見を次世代へ引き継ぐ。属人化リスクを解消する“知識継承AI” SkillRelay</p>
+      </div>
+
+      <div class="button_content_flex">
+        <div class="dl_button_content">
+          <a class="dl_button_bgimg" href="#">
+            <span class="dl_text">ホワイトペーパーを無料DL</span>
+            <img class="icon_img" src="./img/pc/グループ 491 1.png" />
+          </a>
+        </div>
+        <div class="donu_button_content">
+          <div class="donu_button_rectangle">
+            <a class="donu_text" href="#">導入に向けた個別相談をする</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="chat_img_content">
+        <img src="./img/pc/グループ 492 1.png" />
+      </div>
+
+      <div class="blue_rectangle_content">
+        <div class="blue_rectangle"></div>
+        <p class="blue_rectangle_text">CHALLENGE</p>
+      </div>
+
+      <div class="usinaware_text_content">
+        <p>失われていく「企業の知」、放置していませんか？</p>
+      </div>
+
+      <div class="beleran_text_content">
+        <p>ベテランの退職や異動で途切れがちなノウハウ。</p>
+        <p>いま資産化に取り組むことで、継続的な成長と競争力強化につなげられます。</p>
+      </div>
+
+      <div class="box_content_flex">
+        <div class="white_box">
+          <img src="./img/pc/グループ 11 1.png" />
+          <p>知見の属人化</p>
+          <span>
+            ベテラン社員の経験や判断基準が個人に依存し、共有されないまま業務がブラックボックス化してしまう。
+          </span>
+        </div>
+        <div class="white_box">
+          <img src="./img/pc/グループ 12 1.png" />
+          <p>継承の限界</p>
+          <span>
+            マニュアルや文書に残らない暗黙知が多く、異動・退職のたびにノウハウが途切れてしまう。
+          </span>
+        </div>
+        <div class="white_box">
+          <img src="./img/pc/グループ 13 1.png" />
+          <p>育成コストの増大</p>
+          <span>
+            若手や新任社員の教育に時間とコストがかかり、即戦力化が進まない。
+          </span>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+
+  <section class="solution_section">
+
+  </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <script src="./js/index.js"></script>
+</body>
+
+</html>
