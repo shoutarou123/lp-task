483cf38fc704b8a2bb47b91f68d3957d788d70b9
shotaro <850123.a@gmail.com>
2025-08-26 17:44:24 +0900

    一旦保存

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Protest+Strike&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Protest+Strike&family=Roboto:ital,wght@0,100..900;1,100..900&family=Zen+Kaku+Gothic+New&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
@@ -25,6 +27,8 @@
       </div>
       <div class="catchphrase_img_content">
         <img class="catchphrase_img" src="./img/pc/キャッチコピー@2x 2.png" />
+        <img class="catchphrase_img hidden" src="./img/sp/25_スキルリレー_design 1.png" />
+
       </div>
       <div class="intai_text_content">
         <p class="intai_text">引退・異動で失われる前に、知見を次世代へ引き継ぐ。属人化リスクを解消する“知識継承AI” SkillRelay</p>
@@ -44,22 +48,42 @@
         </div>
       </div>
 
-      <div class="chat_img_content">
-        <img src="./img/pc/グループ 492 1.png" />
+      <div class="position_content">
+        <div class="chat_img_content">
+          <img src="./img/pc/グループ 492 1.png" />
+          <img class="chat_img hidden" src="./img/sp/グループ 20 2.png" />
+        </div>
+
+        <div class="button_content_flex_sp hidden">
+
+          <div class="bg_blue_button">
+            <p>ホワイトペーパーを無料DL</p>
+          </div>
+
+          <div class="box_button">
+            <p>導入に向けた</p>
+            <p>個別相談をする</p>
+          </div>
+        </div>
       </div>
 
-      <div class="blue_rectangle_content">
-        <div class="blue_rectangle"></div>
-        <p class="blue_rectangle_text">CHALLENGE</p>
-      </div>
+      <div class="flex">
 
-      <div class="usinaware_text_content">
-        <p>失われていく「企業の知」、放置していませんか？</p>
-      </div>
+        <div class="section_text_content">
+          <div class="blue_rectangle_content">
+            <div class="blue_rectangle"></div>
+            <p class="blue_rectangle_text">CHALLENGE</p>
+          </div>
 
-      <div class="beleran_text_content">
-        <p>ベテランの退職や異動で途切れがちなノウハウ。</p>
-        <p>いま資産化に取り組むことで、継続的な成長と競争力強化につなげられます。</p>
+          <div class="usinaware_text_content">
+            <p>失われていく「企業の知」、放置していませんか？</p>
+          </div>
+
+          <div class="beteran_text_content">
+            <p>ベテランの退職や異動で途切れがちなノウハウ。</p>
+            <p>いま資産化に取り組むことで、継続的な成長と競争力強化につなげられます。</p>
+          </div>
+        </div>
       </div>
 
       <div class="box_content_flex">
@@ -91,6 +115,84 @@
   </section>
 
   <section class="solution_section">
+
+    <div class="blue_rectangle_content solution">
+      <div class="blue_rectangle"></div>
+      <p class="blue_rectangle_text white">SOLUTION</p>
+    </div>
+
+    <div class="usinaware_text_content solution">
+      <p>その課題、AIナレッジ基盤「SkillRelay」で解決します。</p>
+    </div>
+
+    <div class="beteran_text_content solution">
+      <p>属人化したノウハウを可視化・体系化し、誰もが使える資産へ。 </p>
+      <p>熟練者の判断や経験を組織全体で共有し、継続的な成長と競争力強化を実現します。</p>
+    </div>
+
+
+    <div class="rectangle_solution_contents_flex">
+
+      <div class="rectangle_solution">
+        <div class="rectangle_solution_flex">
+          <div class="check_img_content">
+            <img src="./img/pc/パス 35 1.png" />
+          </div>
+          <div class="rectangle_solution_text_content">
+            <p>ノウハウの可視化</p>
+            <span>暗黙知をAIが整理し、誰でも理解できる形式に変換します。</span>
+          </div>
+        </div>
+
+        <div class="rectangle_solution_flex">
+          <div class="check_img_content">
+            <img src="./img/pc/パス 35 1.png" />
+          </div>
+          <div class="rectangle_solution_text_content">
+            <p>リスクの低減</p>
+            <span>知識の属人化や引き継ぎミスを防ぎ、組織としての安定性を高めます。</span>
+          </div>
+        </div>
+
+        <div class="rectangle_solution_flex">
+          <div class="check_img_content">
+            <img src="./img/pc/パス 35 1.png" />
+          </div>
+          <div class="rectangle_solution_text_content">
+            <p>スピーディな展開</p>
+            <span>熟練者の知見を即座に活用できる状態にし、現場での判断力を支援します。</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="earth_content_block">
+
+        <div class="earth_bg_img">
+          <div class="fukidashi_content">
+            <div class="fukidashi_bg_img">
+              <p>
+                熟練者のノウハウが失われない
+              </p>
+            </div>
+            <div class="fukidashi_bg_img two">
+              <p>
+                知識が共有され組織が強くなる
+              </p>
+            </div>
+            <div class="fukidashi_bg_img three">
+              <p>
+                教育・育成のコストを削減できる
+              </p>
+            </div>
+
+          </div>
+
+
+        </div>
+      </div>
+
+    </div>
+
 
   </section>
 
